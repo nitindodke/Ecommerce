622a468e46aaec8cdba8fb64747db956da6d1594
Nitin Dodke <nitin.dodke@Yash.com>
2022-10-11 18:35:16 +0530

    added getAllProduct API

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,16 @@
+const Product = require("../model/productModel")
+
+// create Product
+
+exports.createProduct = async (req,res,next)=>{
+     const product = await Product.create(req.body);
+
+     res.status(201).json({
+          success:true,
+          product
+     })
+}
+
 exports.getAllProducts = (req,res) =>{
      res.status(200).json({massage:"Route is working fine"})
 }
